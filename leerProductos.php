ae58c41fb77be440ae9e5714b13cf4e02377132a
Keldo44 <82040605+Keldo44@users.noreply.github.com>
2023-11-07 19:32:36 +0100

    c

--- leerProductos.php
+++ leerProductos.php
@@ -22,98 +22,93 @@ $con=mysqli_connect($servidor,$usuario,$password,$bd);
 
 if($_SESSION['admin'] == 1){
   if($con){
-    
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Leer Formulario</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+          <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>    
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+          <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+      </head>
+      <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#"><?=$_SESSION['usuario']?></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="main.php">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="leerProductos.php">Leer productos</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="actualizarProductos.php">Actualizar productos</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="borrarProductos.php">Borrar productos</a>
+                      </li>
+                    </ul>
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Buscador por ID o Nombre" name="buscador" aria-label="Buscador por ID">
+                      <button class="btn btn-outline-light" type="submit" name="" style="margin-right:10px ;">Buscar</button>
+                    </form>
+                    <button class="delete btn btn-outline-light" type="submit"><a style="text-decoration: none; color:white" onclick="cerrarSesion(event)" 
+                    href="Login.php">Logout</a></button>
+                </div>
+              </div>
+        </nav>
+      <div class="container mt-3">
+      <h1 style="text-align:center">Listado de productos</h1>
+      <h3 style="text-align:center">Si desea añadir al carrito presione en cualquiera de las filas</h3>
+        <table class="table table-dark table-hover">
+          <tbody>
+            <tr>
+              <th>ID Producto</th>
+              <th>Nombre Producto</th>
+              <th>Cantidad</th>
+              <th>Precio</th>
+              <th>Descripción</th>
+            </tr>
+            <?php
+            $buscador = $_GET['buscador']; 
+            //var_dump($buscador);
+            if($buscador != NULL ||  $buscador != ""){
+              buscador($con,$buscador);
+            }else{
+              //var_dump($_GET['buscador']);
+              $sql2="SELECT * FROM `productos`";
+              $consulta=mysqli_query($con,$sql2);
+              while($fila=$consulta->fetch_assoc()){
+                  echo "<tr>";
+                  echo "<td>".$fila["id"]."</td>";
+                  echo "<td>".$fila["nombre"]."</td>";
+                  echo "<td>".$fila["cantidad"]."</td>";
+                  echo "<td>".$fila["precio"]."</td>";
+                  echo "<td>".$fila["descripcion"]."</td>";
+                  echo "</tr>";
+              }
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
 
-?>
+          <form action="/logout.php" method="get">
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Leer Formulario</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-      <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>    
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#"><?=$_SESSION['usuario']?></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="main.php">Home</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="leerProductos.php">Leer productos</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="actualizarProductos.php">Actualizar productos</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="borrarProductos.php">Borrar productos</a>
-                  </li>
-                </ul>
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Buscador por ID o Nombre" name="buscador" aria-label="Buscador por ID">
-                  <button class="btn btn-outline-light" type="submit" name="" style="margin-right:10px ;">Buscar</button>
-                </form>
-                <button class="delete btn btn-outline-light" type="submit"><a style="text-decoration: none; color:white" onclick="cerrarSesion(event)" 
-                href="Login.php">Logout</a></button>
-            </div>
-          </div>
-    </nav>
-  <div class="container mt-3">
-  <h1 style="text-align:center">Listado de productos</h1>
-  <h3 style="text-align:center">Si desea añadir al carrito presione en cualquiera de las filas</h3>
-    <table class="table table-dark table-hover">
-      <tbody>
-        <tr>
-          <th>ID Producto</th>
-          <th>Nombre Producto</th>
-          <th>Cantidad</th>
-          <th>Precio</th>
-          <th>Descripción</th>
-        </tr>
-        <?php
-        $buscador = $_GET['buscador']; 
-        //var_dump($buscador);
-        if($buscador != NULL ||  $buscador != ""){
-          buscador($con,$buscador);
-        }else{
-          //var_dump($_GET['buscador']);
-          $sql2="SELECT * FROM `productos`";
-          $consulta=mysqli_query($con,$sql2);
-          while($fila=$consulta->fetch_assoc()){
-              echo "<tr>";
-              echo "<td>".$fila["id"]."</td>";
-              echo "<td>".$fila["nombre"]."</td>";
-              echo "<td>".$fila["cantidad"]."</td>";
-              echo "<td>".$fila["precio"]."</td>";
-              echo "<td>".$fila["descripcion"]."</td>";
-              echo "</tr>";
-          }
-        }
-        ?>
-      </tbody>
-    </table>
-  </div>
-
-      <form action="/logout.php" method="get">
-
-  </form>
-  </body>
-</html>
-
-<?php
-  }
-}else{
+      </form>
+      </body>
+    </html>
+    <?php
+  }}else{
   ?>
   <!DOCTYPE html>
   <html lang="en">
@@ -182,7 +177,10 @@ if($_SESSION['admin'] == 1){
                 <td><?=$fila['precio']?></td>
                 <td><?=$fila['descripcion']?></td>
                 <td>
-                  <button type="submit" style="color:black;font-size:20px;padding-right:5%" value = "<?=$fila['id']?>" iduser = "<?=$_SESSION['usuario']?>" name="btnCarro">Añadir</button>
+                  <button type="submit" style="color:black;font-size:20px;padding-right:5%" 
+                    value = "<?=$fila['id']?>" iduser = "<?=$_SESSION['usuario']?>" name="btnCarro">
+                    Añadir
+                  </button>
                 </td>
                 </tr>
               <from>
--- main.php
+++ main.php
@@ -1,80 +1,79 @@
 <?php
 session_start();
-$_SESSION['id'];
+$_SESSION["id"];
 $_SESSION['usuario'];
 $_SESSION['admin'];
+var_dump($_SESSION);
 
 if($_SESSION['admin'] == 1){
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>  
+        <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>  
-    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#"><?=$_SESSION['usuario']?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-            <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="main.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="leerProductos.php">Leer productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="actualizarProductos.php">Actualizar productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="borrarProductos.php">Borrar productos</a>
-                </li>
-                </ul>
-                <button class="delete btn btn-outline-light" type="submit"><a style="text-decoration: none; color:white" onclick="cerrarSesion(event)" 
-              href="Login.php">Logout</a></button>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#"><?=$_SESSION['usuario']?></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="main.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="leerProductos.php">Leer productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="actualizarProductos.php">Actualizar productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="borrarProductos.php">Borrar productos</a>
+                    </li>
+                    </ul>
+                    <button class="delete btn btn-outline-light" type="submit"><a style="text-decoration: none; color:white" onclick="cerrarSesion(event)" 
+                href="Login.php">Logout</a></button>
+            </div>
+        </nav>
+        <div class="container mt-3">
+            <h2>Añadir productos</h2>
+            <form action="consultas.php" method="get">
+                <div class="mb-3 mt-3">
+                    <label for="">Nombre del producto:</label>
+                    <input type="text" class="form-control" id="nombre" placeholder="Nombre del producto" name="nombre">
+                </div>
+                <div class="mb-3">
+                    <label for="">Precio del producto:</label>
+                    <input type="number" class="form-control" id="precio" placeholder="Precio del producto" name="precio">
+                </div>
+                <div class="mb-3">
+                    <label for="">Cantidad:</label>
+                    <input type="number" class="form-control" id="cantidad" placeholder="Cantidad" name="cantidad">
+                </div>
+                <div class="mb-3">
+                    <label for="">Descripcion:</label>
+                    <input type="text" class="form-control" id="descripcion" placeholder="Descripcion" name="descripcion">
+                </div>
+                <div class="mb-3 row" style="justify-content:center">
+                    <button type="submit" class="btn btn-dark col-6" name = "insertBtn" >Enviar</button>
+                <div>
+            </form>
         </div>
-  </nav>
-    <div class="container mt-3">
-        <h2>Añadir productos</h2>
-        <form action="consultas.php" method="get">
-            <div class="mb-3 mt-3">
-                <label for="">Nombre del producto:</label>
-                <input type="text" class="form-control" id="nombre" placeholder="Nombre del producto" name="nombre">
-            </div>
-            <div class="mb-3">
-                <label for="">Precio del producto:</label>
-                <input type="number" class="form-control" id="precio" placeholder="Precio del producto" name="precio">
-            </div>
-            <div class="mb-3">
-                <label for="">Cantidad:</label>
-                <input type="number" class="form-control" id="cantidad" placeholder="Cantidad" name="cantidad">
-            </div>
-            <div class="mb-3">
-                <label for="">Descripcion:</label>
-                <input type="text" class="form-control" id="descripcion" placeholder="Descripcion" name="descripcion">
-            </div>
-            <div class="mb-3 row" style="justify-content:center">
-                <button type="submit" class="btn btn-dark col-6" name = "insertBtn" onclick="notificarEnvio()">Enviar</button>
-            <div>
-        </form>
-    </div>
-</body>
-</html>
-<?php
+    </body>
+    </html>
+    <?php
 }
 ?>
 <script type="text/javascript">
